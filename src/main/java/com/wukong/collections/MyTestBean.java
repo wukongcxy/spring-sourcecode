9dcf0b946af9838ff792e4b4384241c0c4be3c4f
chenxinyue <chenxinyue@baidu.com>
2016-04-15 09:59:25 +0800

    litter change

--- src/main/java/com/wukong/collections/MyTestBean.java
+++ src/main/java/com/wukong/collections/MyTestBean.java
@@ -4,6 +4,10 @@ package com.wukong.collections;
  * Created by chenxinyue on 2016/4/12.
  */
 public class MyTestBean {
+    public MyTestBean() {
+        System.out.println("construct MyTestBean!");
+    }
+
     public void sayHello() {
         System.out.println("Hello beans!");
     }
