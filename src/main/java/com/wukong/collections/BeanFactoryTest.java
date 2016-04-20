4ffda1bf118c7e61dce573788f39e89081992c43
chenxinyue <chenxinyue@baidu.com>
2016-04-20 09:41:22 +0800

    developer double hello

--- src/main/java/com/wukong/collections/BeanFactoryTest.java
+++ src/main/java/com/wukong/collections/BeanFactoryTest.java
@@ -11,6 +11,7 @@ public class BeanFactoryTest {
     public static void main(String[] args) {
         BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
         MyTestBean bean = bf.getBean("myTestBean", MyTestBean.class);
+        bean.sayHello();bean.sayHello();
         bean.sayHello();
 
     }
